8d9513e3b5fdae8bf1999a6842bdf17afc2c77e7
sachin.suryavanshi@accenture.com <suryavanshisachin380@gmail.com>
2025-04-26 22:52:15 +0530

    create- menu, coniguration controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MenuController } from './menu/menu.controller';
+import { ConfigurationController } from './configuration/configuration.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, MenuController, ConfigurationController],
   providers: [AppService],
 })
 export class AppModule {}
